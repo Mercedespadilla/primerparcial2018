9444c8473efc973b57ce4271f685211b1f968368
ufg <ufg@ufg-HP>
2018-05-04 09:16:37 -0600

    agregando la clase Expaquete que contiebe los servicios

--- Parcial/src/parcial/Expaquete.java
+++ Parcial/src/parcial/Expaquete.java
@@ -7,8 +7,21 @@ package parcial;
 
 /**
  *
- * @author ufg
+ * @author mercedes padilla
  */
 public class Expaquete {
-    
+    public String detalle;
+
+    public Expaquete(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
 }
