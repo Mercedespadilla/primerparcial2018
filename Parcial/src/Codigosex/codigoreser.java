7c78135eb7c20191c0875fa3f2e0fdb2f1fc3716
Mercedespadilla <00186416@uca.edu.sv>
2018-05-04 09:28:55 -0600

    codigo con funciones

---
+++ Parcial/src/Codigosex/codigoreser.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Codigosex;
+import java.util.ArrayList;
+import java.util.Scanner;
+import parcial.RegistroReserva;
+import parcial.Paquete;
+/**
+ *
+ * @author mercedes padilla
+ */
+public class codigoreser {
+   private void crearReserva(RegistroReserva[] reservahotel ) {
+         Scanner entradaEscaner = new Scanner (System.in);
+         System.out.println ("Por favor introduzca el nombre del huesped:");
+         String codigoreserva = entradaEscaner.nextLine ();  
+}
