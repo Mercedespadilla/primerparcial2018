64659d611efe91925e44b64723e3ae08e972e614
Mercedespadilla <00186416@uca.edu.sv>
2018-05-04 09:02:11 -0600

    Agregando clase Paquete

--- Parcial/src/parcial/Paquete.java
+++ Parcial/src/parcial/Paquete.java
@@ -5,10 +5,49 @@
  */
 package parcial;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author ufg
+ * @author alejandra padilla
  */
 public class Paquete {
+     public String nombre;
+    public double precio;
+    public ArrayList<String> servicios;
+
+    public Paquete() {
+    }
+
+    public Paquete(String nombre, double precio, ArrayList<String> servicios) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.servicios = servicios;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public ArrayList<String> getServicios() {
+        return servicios;
+    }
+
+    public void setServicios(ArrayList<String> servicios) {
+        this.servicios = servicios;
+    }
     
 }
+
