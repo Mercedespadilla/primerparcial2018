2369d8fbc943808e54a3c5db5ce9bab9e74165e8
Monica Gomez <Monica Gomez@DESKTOP-O462FC8>
2018-05-04 22:53:17 -0600

    Commit 08:50 PM

--- Parcial/src/parcial/Huesped.java
+++ Parcial/src/parcial/Huesped.java
@@ -3,117 +3,87 @@
  */
 package parcial;
 
-import java.util.Scanner;
-
-
 public class Huesped {
 
-    private static void crearHuesped() {
-        String strNombre = "";
-        String strApellido = "";
-        String strEdad  = "";
-        String strPais  = "";
-        String strTelefono  = "";
-        String strTarjeta  = "";
-        Scanner entradaEscaner = new Scanner (System.in); 
-        System.out.println ("Por favor introduzca el nombre del huesped:");
-        strNombre = entradaEscaner.nextLine (); 
-        System.out.println ("Por favor introduzca el apellido del huesped:");
-        strApellido = entradaEscaner.nextLine (); 
-        System.out.println ("Por favor introduzca la edad del huesped:");
-        strEdad = entradaEscaner.nextLine ();
-        System.out.println ("Por favor introduzca el pais de residencia del huesped:");
-        strPais = entradaEscaner.nextLine ();
-        System.out.println ("Por favor introduzca el telefono del huesped:");
-        strTelefono = entradaEscaner.nextLine ();
-        System.out.println ("Por favor introduzca el numero de tarjeta del huesped:");
-        strTarjeta = entradaEscaner.nextLine ();
+    private String strNombre;
+    private String strApellido;
+    protected int intEdad;
+    private String strPais;
+    private String strTelefono;
+    private String strTarjeta;
+
+    public static void main(String[] args) {
+    
+    }
+    
+    public void VerHuesped()
+    {
+    System.out.print("\nNombre:"+this.strNombre+
+    "\nApellidos:"+this.strApellido+
+    "\nEdad:"+this.intEdad+
+    "\nPais:"+this.strPais+
+    "\nTelefono:"+this.strTelefono+
+    "\nTarjeta:"+this.strTarjeta);
+    }
+    
+    public Huesped(String strNombre, String strApellido, int intEdad, String strPais, String strTelefono, String strTarjeta) {
+        this.strNombre = strNombre;
+        this.strApellido = strApellido;
+        this.intEdad = intEdad;
+        this.strPais = strPais;
+        this.strTelefono = strTelefono;
+        this.strTarjeta = strTarjeta;
+    }
+    
+    public Huesped() {
+    }
+    
+    public String getNombre() {
+        return strNombre;
     }
 
-    private static void modificarHuesped() {
-        String strNombre = "";
-        String strApellido = "";
-        String strEdad  = "";
-        String strPais  = "";
-        String strTelefono  = "";
-        String strTarjeta  = "";
-        Scanner entradaEscaner = new Scanner (System.in); 
-        System.out.println ("Por favor introduzca el nombre del huesped:");
-        strNombre = entradaEscaner.nextLine (); 
-        System.out.println ("Por favor introduzca el apellido del huesped:");
-        strApellido = entradaEscaner.nextLine (); 
-        System.out.println ("Por favor introduzca la edad del huesped:");
-        strEdad = entradaEscaner.nextLine ();
-        System.out.println ("Por favor introduzca el pais de residencia del huesped:");
-        strPais = entradaEscaner.nextLine ();
-        System.out.println ("Por favor introduzca el telefono del huesped:");
-        strTelefono = entradaEscaner.nextLine ();
-        System.out.println ("Por favor introduzca el numero de tarjeta del huesped:");
-        strTarjeta = entradaEscaner.nextLine ();
+    public void setNombre(String Nombre) {
+        this.strNombre = Nombre;
+    }
+
+    public String getApellido() {
+        return strApellido;
+    }
+
+    public void setApellido(String Apellidos) {
+        this.strApellido = Apellidos;
+    }
+    
+    public int getEdad() {
+        return intEdad;
+    }
+
+    public void setEdad(int Edad) {
+        this.intEdad = Edad;
+    }
+    
+    public String getPais() {
+        return strPais;
+    }
+
+    public void setPais(String Pais) {
+        this.strPais = Pais;
     }    
     
-        private static void eliminarHuesped() {
-        String strNombre = "";
-        String strApellido = "";
-        String strEdad  = "";
-        String strPais  = "";
-        String strTelefono  = "";
-        String strTarjeta  = "";
-        Scanner entradaEscaner = new Scanner (System.in); 
-        System.out.println ("Por favor introduzca el nombre del huesped:");
-        strNombre = entradaEscaner.nextLine (); 
-        System.out.println ("Por favor introduzca el apellido del huesped:");
-        strApellido = entradaEscaner.nextLine (); 
-        System.out.println ("Por favor introduzca la edad del huesped:");
-        strEdad = entradaEscaner.nextLine ();
-        System.out.println ("Por favor introduzca el pais de residencia del huesped:");
-        strPais = entradaEscaner.nextLine ();
-        System.out.println ("Por favor introduzca el telefono del huesped:");
-        strTelefono = entradaEscaner.nextLine ();
-        System.out.println ("Por favor introduzca el numero de tarjeta del huesped:");
-        strTarjeta = entradaEscaner.nextLine ();
+    public String getTelefono() {
+        return strTelefono;
     }
+
+    public void setTelefono(String Telefono) {
+        this.strTelefono = Telefono;
+    }      
     
-    private String nombre;
-    private String apel1;
-    private String apel2;
-    private String dni;
-    
-    public static void main(String[] args) {
-           Scanner sn = new Scanner(System.in);
-       boolean salir = false;
-       int opcion; //Guardaremos la opcion del usuario
-        
-       while(!salir){
-            
-           System.out.println("1. Opcion 1 Crear Huesped");
-           System.out.println("2. Opcion 2 Modificar Huesped");
-           System.out.println("3. Opcion 3 Eliminar Huesped");
-           System.out.println("4. Salir");
-            
-           System.out.println("Escribe una de las opciones");
-           opcion = sn.nextInt();
-            
-           switch(opcion){
-               case 1:
-                   System.out.println("Has seleccionado la opcion 1");
-                   crearHuesped();
-                   break;
-               case 2:
-                   System.out.println("Has seleccionado la opcion 2");
-                   modificarHuesped();
-                   break;
-                case 3:
-                   System.out.println("Has seleccionado la opcion 3");
-                   eliminarHuesped();
-                   break;
-                case 4:
-                   salir=true;
-                   break;
-                default:
-                   System.out.println("Solo números entre 1 y 4");
-           }
-        }    
+    public String getTarjeta() {
+        return strTarjeta;
     }
+
+    public void setTarjeta(String Tarjeta) {
+        this.strTarjeta = Tarjeta;
+    }          
 }
   
